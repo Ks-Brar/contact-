effb3fcfb1c6d1cb20b54658fa41e9e3279869f2
Ks-Brar <84464673+Ks-Brar@users.noreply.github.com>
2023-10-14 14:09:51 +0530

    Update stats.controller.js

--- stats.controller.js
+++ stats.controller.js
@@ -3,7 +3,26 @@ const Phones = db.phones;
 const Contacts = db.contacts;
 const Op = db.Sequelize.Op;
 
-// Calculate stats
-exports.calculate = (req, res) => {
-    
+exports.calculate = async (req, res) => {
+    try {
+        const totalContacts = await db.contacts.count();
+        const totalPhones = await db.phones.count();
+        const latestContact = await db.contacts.findOne({
+            order: [['createdAt', 'DESC']]
+        });
+        const oldestContact = await db.contacts.findOne({
+            order: [['createdAt', 'ASC']]
+        });
+
+        res.json({
+            totalContacts,
+            totalPhones,
+            latestContactTime: latestContact ? latestContact.createdAt : null,
+            oldestContactTime: oldestContact ? oldestContact.createdAt : null
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: "Error retrieving stats"
+        });
+    }
 };
